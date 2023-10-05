9a4435227fa6a6ef04bbb9fbde9236c4d3db3d1c
cerilizpb <cerilizpbrillantes@gmail.com>
2023-10-05 13:49:34 +0800

    commit 2

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,4 +1,6 @@
-describe('User Registration', () => {
+// registration.spec.js
+
+describe('TC1.1 User Registration', () => {
   it('should register a new user', () => {
     cy.visit('https://stage.spinbet.com/en-nz'); // Visit the registration page
 
@@ -18,9 +20,40 @@ describe('User Registration', () => {
   });
 });
 
+describe('TC1.2: Empty Registration Form Submission', () => {
+  it('should not allow submission of empty registration form', () => {
+    cy.visit('https://stage.spinbet.com/en-nz');
+    
+    // Locate registration form fields and submit button
+    cy.get('[data-testid="registration-form"]').within(() => {
+      // Locate and submit the form without filling any fields
+      cy.get('[data-testid="register-button"]').click();
+    });
+    
+    // Assert error messages or behavior when the form submission fails
+    cy.get('[data-testid="error-message"]').should('exist');
+  });
+});
+
+describe('TC1.3: Registration Form Validation-Invalid', () => {
+    it('Should show error for invalid email format', () => {
+        cy.visit('https://stage.spinbet.com/en-nz'); // Visit the registration page
+
+        // Fill out the registration form with an invalid email address
+        cy.get('[data-testid=registration-email-input]').type('invalidemail'); // Invalid email format
+
+        // Submit the form
+        cy.get('[data-testid=registration-submit-button]').click();
+
+        // Assert that an error message is displayed for the email input field
+        cy.get('[data-testid=registration-email-error-message]').should('be.visible');
+        cy.get('[data-testid=registration-email-error-message]').should('contain', 'Invalid email address format');
+    });
+});
+
 
 // casino_search_spec.js
-describe('Casino Search Functionality', () => {
+describe('TC2.1 Casino Search Functionality', () => {
   beforeEach(() => {
       cy.visit('https://stage.spinbet.com/en-nz/casino');
   });
@@ -37,5 +70,37 @@ describe('Casino Search Functionality', () => {
 
       // Validate the search results
       cy.get('.search-result-item').should('have.length.greaterThan', 0);
+  });
+});
+
+escribe('TC2.2 Casino Lobby Empty Search', () => {
+  it('should validate empty search functionality', () => {
+    // Visit the Casino Lobby page
+    cy.visit('https://stage.spinbet.com/en-nz/casino');
+    
+    // Locate the search input field and submit the empty search
+    cy.get('.search-input').type('{enter}');
+    
+    // Validate that no search results are displayed
+    cy.get('.search-results').should('not.exist');
+    
+    // Optionally, you can also validate the presence of a message indicating no search results found
+    cy.contains('No results found').should('exist');
+  });
+});
+
+
+scribe('TC2.3 Search Functionality - Invalid', () => {
+  it('should validate search with special characters', () => {
+    // Visit the casino lobby page
+    cy.visit('https://stage.spinbet.com/en-nz/casino');
+
+    // Perform a search with special characters
+    const specialCharacter = '@#$%';
+    cy.get('[data-cy=search-input]').type(specialCharacter);
+    cy.get('[data-cy=search-button]').click();
+
+    // Validate search results
+    cy.get('[data-cy=search-results]').should('contain', 'No results found');
   });
 });
